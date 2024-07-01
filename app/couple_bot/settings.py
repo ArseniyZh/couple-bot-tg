ad4be0700f6e581bc0b82498f053c1710e911a24
a.zhelvakov <a.zhelvakov@otus.ru>
2024-07-02 01:04:56 +0300

    fix

--- app/couple_bot/settings.py
+++ app/couple_bot/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "couple_bot.wsgi.application"
 DB_NAME = env.str("DB_NAME")
 DB_USER = env.str("DB_USER")
 DB_PASSWORD = env.str("DB_PASSWORD")
-DB_HOST = "localhost"  # env.str("DB_HOST")
+DB_HOST = env.str("DB_HOST")
 DB_PORT = env.str("DB_PORT")
 DATABASES = {
     "default": {
